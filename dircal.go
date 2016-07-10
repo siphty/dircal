817f0b2b0340fb1f3f54c5373c66f64fe7d60308
JIANG YI <Robert.yi.jones@gmail.com>
2016-07-10 22:54:18 +1000

    Update dircal.go

--- dircal.go
+++ dircal.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	fmt.Printf("Hello, world.\n")
 	var dateString string
-	for i := 0; i < 100; i++ {
+	var startDate = time.Now().AddDate(0,0,-14)
+	for i := 0; i < 300; i++ {
 		//fmt.Println("Count number: %d ", i)
-		date := time.Now().AddDate(0, 0, i)
+		date := startDate.AddDate(0, 0, i)
 		dateString = fmt.Sprint(date.Format("2006-01-02"))
 		err := os.MkdirAll(dateString, 0777)
 		if err != nil {
